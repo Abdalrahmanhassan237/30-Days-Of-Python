c5dc94597ec3ac1e943b6d5a28c871a875d8d00f
AbdAlrahman Hassan <abdalrahman.hassan237@gmail.com>
2024-04-18 19:55:45 +0200

    add Day 1 folder

---
+++ 01_Day_Introduction/Python as calculator.py
@@ -0,0 +1,28 @@
+'''' Python is perfectly suited to do basic calculations.
+ It can do addition, subtraction, multiplication and division. '''
+
+# Arithmetic Operator
+
+# Addition
+print(4 + 5)
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10 / 2)
+
+# floor Division
+
+print(10 // 3)
+
+# modules
+
+print(10 % 3)
+
+# The Exponent ** Operator
+
+print(2 ** 3)
