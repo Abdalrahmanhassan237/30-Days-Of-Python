db5a38e1ab77ef84651e64dba5aa44b4a5988e94
AbdAlrahman Hassan <abdalrahman.hassan237@gmail.com>
2024-04-21 17:36:10 +0200

    update python as calculator

--- 01_Day_Introduction/Python as calculator.py
+++ 01_Day_Introduction/Python as calculator.py
@@ -1,28 +1,61 @@
 '''' Python is perfectly suited to do basic calculations.
  It can do addition, subtraction, multiplication and division. '''
 
+# Arithmetic Operators    +, - , / ,// *, %, %%
+
+
+x = -5       # Negates the value of X
+z = +-x      # +(-5) ---> 5
+print(z)
+
+print(1 -- 3)   # Equivalent to 1-(-3)
+print(2 --- 4)  # Equivalent to 2-(-(-3))
+print(3 +-- 2)  # Equivalent to 3+(-(-2))
+
 # Arithmetic Operator
 
 # Addition
+
+# it returns the result as the arithmetic sum of the operands
+
+
 print(4 + 5)
 
 # Subtraction
+
+#  it returned as the arithmetic difference of the operands
+
 print(5 - 5)
 
 # Multiplication
+
+# it returns the result as the arithmetic product of the operands
+
 print(3 * 5)
 
 # Division
+
+# it returns the result as the arithmetic quotient of the operands.
+
+
 print(10 / 2)
+print(5.45 / 9.8)
+
+# When the division (/) operator is applied on two int operands, Python returns a float resul
 
 # floor Division
 
-print(10 // 3)
+# it returns the result as the arithmetic quotient of the operands
+
+print(10 // 3)     # output : 3   not 3.333333333333333333333
 
 # modules
 
-print(10 % 3)
+print(10 % 3)      # X%Y is equivalent to X – Y * (x//Y)
 
 # The Exponent ** Operator
 
-print(2 ** 3)
+print(2 ** 3)     # output 8
+
+
+
